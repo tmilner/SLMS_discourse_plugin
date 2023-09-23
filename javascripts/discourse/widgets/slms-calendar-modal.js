6d1a3f7f7b2ccabb1449843c764dd1cb63b742e0
Tom Milner <mail@tmilner.co.uk>
2023-09-23 10:18:51 +0100

    Test opening the composer window

--- javascripts/discourse/widgets/slms-calendar-modal.js
+++ javascripts/discourse/widgets/slms-calendar-modal.js
@@ -1,22 +1,27 @@
 import { createWidget } from "discourse/widgets/widget";
 import { h } from "virtual-dom";
-import panelMessage from "../lib/panel-message";
-import getTheme from "../lib/theme";
-import DiscourseURL from "discourse/lib/url";
 
 createWidget("slms-calendar-modal", {
   tagName: "div.slms-calendar-panel",
 
-  html() {
-      return this.attach("menu-panel", {
-        contents: () =>
-          panelMessage(
-            "warning",
-            "slms_calendar_widget.temp_msg",
-            "exclamation-triangle"
-          ),
+  click(event) {
+    let $target = $(event.target);
+    let id = $target.data("id");
+    if (id == "create") {
+      this.container.lookup("controller:composer").open({
+        action: Composer.CREATE_TOPIC,
+        draftKey: Composer.NEW_TOPIC_KEY,
       });
-    
+    }
+  },
+
+  html(attrs, state) {
+    let buttonHtml = h(
+      "li",
+      { attributes: { "data-name": "Create" } },
+      h("a.widget-link", { attributes: { "data-id": "create" } }, "Create")
+    );
+    return [h("ul.menu-links.columned", [buttonHtml]), h(".clearfix"), h("hr")];
   },
 
   clickOutside() {
